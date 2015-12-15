865e5461b336ef8998c3fecc9eff7219bd00c262
Honan <jrp12@hampshire.edu>
2015-12-14 22:59:29 -0500

    basic transition working

--- index.ios.js
+++ index.ios.js
@@ -11,17 +11,72 @@ var {
   StyleSheet,
   Text,
   View,
+  Navigator,
   StatusBarIOS,
 } = React;
 
+var buildStyleInterpolator = require('buildStyleInterpolator');
+
+
+var { Dimensions } = React;
+var SCREEN_WIDTH = Dimensions.get('window').width;
+var BaseConfig = Navigator.SceneConfigs.FloatFromRight;
+
+var BaseLeftToRightGesture = Navigator.SceneConfigs.BaseLeftToRightGesture;
+var BaseRightToLeftGesture = Navigator.SceneConfigs.BaseRightToLeftGesture;
+
+var BaseOverswipeConfig = Navigator.SceneConfigs.BaseOverswipeConfig;
+
+
+
+var CustomSceneConfig = Object.assign({}, Navigator.SceneConfigs.HorizontalSwipeJump, {
+  springFriction: 0,
+  springTension: 8,
+  defaultTransitionVelocity: 1.5,
+
+});
 
 //main theme color 3fb7be
 
+var TextMessage = React.createClass({
+  setNativeProps(nativeProps) {
+    this._root.setNativeProps(nativeProps);
+  },
+  render: function(){
+    var { text, ...other } = this.props;
 
-var PHSBS = React.createClass({
-  statics: {
-    title: 'Popular High School Boy Simulator',
-    description: 'Your so cool, Austin'
+    return ( 
+      <View ref={component => this._root = component} {...this.props}
+        style={[styles.row, styles.paddMe]}>
+        <View style = {styles.seen}>
+          <Image source={require('./bluecircle.png')}
+          style = {styles.dot}/>
+        </View>
+        <View style = {styles.content}>
+          <View style={styles.row}>
+            <Text style={[styles.text, styles.from, styles.bold]}>
+            {text.from}
+            </Text>
+            <Text style={[styles.text, styles.time]}>
+            {text.time} > 
+            </Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={[styles.text, styles.message]}>
+             {text.message}
+            </Text>
+          </View>
+          <View style={styles.separator} />
+        </View>
+      </View>
+    )
+  }
+})
+
+var Main = React.createClass({
+
+  setNativeProps(nativeProps) {
+    this._root.setNativeProps(nativeProps);
   },
 
   getInitialState: function() {
@@ -39,74 +94,16 @@ var PHSBS = React.createClass({
     this._pressData = {};
   },
 
-
-  render: function() {
-
-    var rightButtonConfig = {
-      title: '+',
-      handler: function onNext() {
-        alert('hello!');
-      }
-
-    };
-
-    var titleConfig = {
-      title: 'Messages',
-    };
-    return (
-        <View>
-          <NavigationBar
-            style = {styles.header}
-            title={titleConfig}
-            rightButton={rightButtonConfig} />
-          <View style={styles.separator} />
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={this._renderRow} />
-
-        </View>
-    );
-  },
-
   _renderRow: function(text: {}, sectionID: number, rowID: number) {
-
     return (
-      <TouchableHighlight onPress={() => this._pressRow(rowID)}>
-        <View>
+      <TouchableHighlight underlayColor = "#ccc" 
+        onPress={() => {
+          this._pressRow(rowID)
+          this.props.textDetail(text.from);
+        }}>
 
-          <View style={[styles.row, styles.paddMe]}>
+        <TextMessage text = {text}/>
 
-            <View style = {styles.seen}>
-              <Image source={require('./bluecircle.png')}
-              style = {styles.dot}/>
-            </View>
-
-            <View style = {styles.content}>
-
-              <View style={styles.row}>
-                <Text style={[styles.text, styles.from, styles.bold]}>
-                {text.from}
-                </Text>
-                <Text style={[styles.text, styles.time]}>
-                {text.time} > 
-                </Text>
-              </View>
-
-              <View style={styles.row}>
-                <Text style={[styles.text, styles.message]}>
-                 {text.message}
-                </Text>
-
-              </View>
-
-              <View style={styles.separator} />
-
-            </View>
-
-
-          </View>
-
-        </View>
       </TouchableHighlight>
     );
   },
@@ -135,6 +132,77 @@ var PHSBS = React.createClass({
       this._genRows(this._pressData)
     )});
   },
+
+  render: function(){
+    var {state, textDetail, onBack, ...other} = this.props;
+
+    const titleConfig = {
+     title: this.props.state,
+    };
+
+    const leftButtonConfig = {
+      title: '< Back',
+      handler: () => onBack(),
+    };
+
+    if(this.props.state == "Messages") return (     
+      <View style = {styles.border} ref={component => this._root = component} {...this.props}>
+        <NavigationBar
+          style = {styles.header}
+          title={titleConfig} />
+        <View style={styles.separator} />
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow} />
+      </View>
+    )
+    else return(
+      <View style = {styles.border} ref={component => this._root = component} {...this.props}>
+        <NavigationBar
+          style = {styles.header}
+          title={titleConfig} 
+          leftButton = {leftButtonConfig}/>
+        <View style={styles.separator} />
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow} />
+      </View>
+    )
+
+    
+  }
+
+})
+
+var PHSBS = React.createClass({
+
+  _configureScene(route){
+    return CustomSceneConfig;
+  },
+
+  render: function() {
+    return (
+      <Navigator
+      initialRoute={{name: 'Messages', index: 0}}
+      configureScene = {this._configureScene}
+      renderScene={(route, navigator) => {
+        return (<Main
+        state={route.name}
+        textDetail={(texter) => {
+          navigator.push({
+            name: texter,
+            index: route.index + 1,
+          });
+        }}
+        onBack={() => {
+          if (route.index > 0) {
+            navigator.pop();
+          }
+
+        }}/>
+      )}}/>
+    );
+  },
 });
 
 var styles = StyleSheet.create({
@@ -159,9 +227,10 @@ var styles = StyleSheet.create({
   },
   message:{
     fontSize:15,
-    height:50,
+    height:39,
     flex:1,
     color:"#888",
+    marginBottom:10,
   },
   content:{
     flex:9
@@ -172,6 +241,10 @@ var styles = StyleSheet.create({
   },
   bold:{
     fontWeight:"700",
+  },
+  border:{
+    borderLeftWidth:1,
+    borderColor:"#BBB",
   },
   header:{
     backgroundColor: '#F6F6F6',
